43bf385edfd38919c4133dea90b9c111f9d801a0
julianknodt <julianknodt@gmail.com>
2021-09-13 13:29:13 -0400

    Add changes

--- App.js
+++ App.js
@@ -1,51 +1,164 @@
 import { StatusBar } from 'expo-status-bar';
-import React, { Component } from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import React, { Component, useState } from 'react';
+import { StyleSheet, Text, TextInput, View, Button } from 'react-native';
 
-// what
-const views = [
-  "splash",
-  "sign_up",
-  "sign_in",
-  "home",
-  "send_msg",
-  "recv_msg",
-];
+// what views are available for the app
+const views = {
+  Splash: "Splash",
+  SignUp: "SignUp",
+  SignIn: "SignIn",
+  Home: "Home",
+  SendMsg: "SendMsg",
+  RecvMsg: "RecvMsg",
+}
 
 class MainApp extends Component {
-  state = {view: 0}
-  set_state_sign_up() {
-    this.setState({view: 1})
+  // react state
+  state = {view: views.Splash}
+  // stack of which views were visited
+  stack = [];
+  // current views state (in theory can put this on the stack too)
+
+  goto_view(view) {
+    this.stack.push(this.state);
+    this.setState({view: view});
   }
-  set_state_sign_in() {
-    this.setState({view: 2})
+  clear_stack() { this.stack = [] };
+  back() {
+    const prev = this.stack.pop();
+    if (prev!=undefined) this.setState(prev);
+  }
+  async login() {
+    // TODO fill this in with a server address and actually use it
+    // const resp = await fetch("http://localhost:8080");
+    // const json = await resp.json();
+    const tempUser = {
+      name: "juju",
+      email: "jknodt@princeton.edu",
+    }
+    this.setState({user: tempUser});
+    this.goto_view(views.Home)
+  }
+  async signup() {
+    throw "NotImplementedError"
+  }
+  validate_email() {
+    const error = (() => {
+      if (!this.state.email) return null
+      if (this.state.email == "") return null;
+      if (!this.state.email.endsWith("princeton.edu")) {
+        return "Only accepting princeton emails currently.";
+      };
+    })();
+    this.setState({email_error: error});
   }
   render() {
+    // TODO when you restart the app you always get sent to the splash screen how to do you make
+    // it so that you get sent to the home screen?
+    const login = this.login.bind(this);
+    const signup = this.signup.bind(this)
+    const back = this.back.bind(this);
+
     const v = views[this.state.view];
-    if (v == "splash")
-      return splash(this.set_state_sign_in.bind(this), this.set_state_sign_up.bind(this));
-    else if (v == "sign_up") return sign_up();
-    else if (v == "sign_in") return sign_in();
+    if (v == views.Splash)
+      return splash(() => this.goto_view(views.SignIn), () => this.goto_view(views.SignUp));
+    else if (v == views.SignUp) return sign_up(
+      back,
+      email => { this.state.email = email; this.validate_email(); },
+      name => { this.state.name = name },
+      () => { this.signup() },
+      this.state.email_error,
+    );
+    else if (v == views.SignIn) return sign_in(
+      back,
+      () => {
+        login().catch(err => alert("Something went wrong 😱!\n" + err))
+      },
+      this.state.email_error,
+    );
+    else if (v == views.Home) return home();
     else throw `Unknown view {v}`;
   }
 }
 
 const splash = (sign_in, sign_up) => (
   <View style={styles.container}>
-    <Text>📭</Text>
-    <Button title="Sign In" onPress={sign_in}>Sign In</Button>
-    <Button title="Sign Up" onPress={sign_up}>Sign Up</Button>
-    <StatusBar style="auto" />
+    <Text>📭📩🙌!</Text>
+    <Button title="Sign In" onPress={sign_in}/>
+    <Button title="Sign Up" onPress={sign_up}/>
+    <StatusBar style="auto"/>
   </View>
 );
 
-const sign_up = () => {
-  throw "Not implemented Error"
-}
+// component for signing up for the app
+const sign_up = (back, set_email, set_name, done, email_error) => (
+  <View style={styles.container}>
+    <Text>Please fill in your email:</Text>
+    <TextInput
+      style={styles.input}
+      keyboardType="email-address"
+      autoCapitalize="none"
+      placeholder="@princeton.edu"
+      onChangeText={set_email}
+    />
+    {email_error && <Text>{email_error}</Text>}
+    <Text>And name:</Text>
+    <TextInput
+      style={styles.input}
+      autoCapitalize="none"
+      placeholder="Hi, my name is: 🥸"
+      onChangeText={set_name}
+    />
+    <Button title="Sign Up" onPress={sign_up}/>
+    <Button title="Back" onPress={back}/>
+  </View>
+);
 
-const sign_in = () => {
-  throw "Not implemented Error"
-}
+// component for signing in to the app.
+const sign_in = (back, login, email_error) => (
+  <View style={styles.container}>
+    <Text>Please fill in your Princeton Email:</Text>
+    <TextInput
+      style={styles.input}
+      keyboardType="email-address"
+      placeholder="@princeton.edu"
+      autoCapitalize="none"
+      onSubmitEditting={login}
+    />
+    {email_error && <Text>{email_error}</Text>}
+    <Button title="Login" onPress={() => { login() }}/>
+    <Button title="Back" onPress={back}/>
+  </View>
+);
+
+const home = () => <View style={styles.container}>
+  <Text>TODO add navigation between sending and receiving messages</Text>
+</View>;
+
+const send_msg = (friends) => <View style={styles.container}>
+  {friends.map(friend => (
+    <Text>{friend.name}</Text>
+    <Button title="Send Msg"/>
+  ))}
+</View>
+
+const draft_msg = send_msg => <View>
+  <TextInput
+    style={styles.input}
+    // TODO automatically bring up emoji picker
+    // keyboardType="email-address"
+    placeholder="Emojis :)"
+    // TODO only accept exactly 3 emojis
+    onChange={validate_emoji}
+  />
+  <Button title="Send" onPress={send_msg}/>
+</View>;
+
+const ack_msg = ack => <View>
+  <Button title="Thumbs Up" onPress={ack}/>
+  <Button title="Hourglass" onPress={ack}/>
+  <Button title="Thumbs Down" onPress={ack}/>
+</View>
 
 export default MainApp;
 
@@ -55,5 +168,12 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+   input: {
+    height: 40,
+    width: 180,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
   },
 });
--- server/main.go
+++ server/main.go
@@ -1,18 +1,23 @@
 package main
 
 import (
-  "net/http"
-  "sync"
-  "fmt"
-  "encoding/binary"
-  "time"
+	"crypto/rand"
+	"encoding/binary"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"strings"
+	"sync"
+	"time"
 )
 
 func main() {
-  // TODO handle certain functions from below
-  s := NewServer()
-  // TODO actually serve the sign in handler
-  s.SignInHandler()
+	// TODO handle certain functions from below
+	s := NewServer()
+	// TODO actually serve the sign in handler
+  if err := s.Serve(); err != nil {
+    fmt.Printf("Server exited: %v", err)
+  }
 }
 
 // TODO have in memory data structures with serialization for persistence
@@ -21,98 +26,131 @@ func main() {
 // universe. For now it should just be a massive struct which contains everything for
 // simplicity.
 type Server struct {
-  // mu guards the map of struct who have signed up
-  mu sync.Mutex
-  SignedUp map[Email]User
-
-  LoggedIn map[Email]LogInToken
+	// mu guards the map of struct who have signed up
+	mu       sync.Mutex
+	SignedUp map[Email]User
+	LoggedIn map[Email]LogInToken
 }
 
 type Email string
+
+func NewEmail(s string) (Email, error) {
+	if s == "" {
+		return Email(""), fmt.Errorf("Cannot make empty string an email")
+	}
+	// XXX temp
+	if !strings.HasSuffix(s, "princeton.edu") {
+		return Email(""), fmt.Errorf("Currently only accepting princeton emails")
+	}
+
+	return Email(s), nil
+}
+
 type User struct {
-  Name string
-  Email string
-  // TODO add other preference fields here
+	Name  string
+	Email Email
+	// TODO add other preference fields here
 }
 
 type LogInToken struct {
-  ValidUntil time.Time
-  // uuid is some unique way of representing a log in token so that it cannot be forged with
-  // just the time.
-  Uuid uint64
-  // awful method of protecting a user, TODO eventually replace this with
+	ValidUntil time.Time
+	// uuid is some unique way of representing a log in token so that it cannot be forged with
+	// just the time.
+	Uuid Uuid
+	// awful method of protecting a user, TODO eventually replace this with
 }
 
 func NewServer() *Server {
-  return &Server{}
+	return &Server{}
 }
 
-func (s *Server) SignUp(userEmail, userName string) error {
-  if _, exists := s.SignedUp[userEmail]; exists {
-    return fmt.Errorf("User already exists")
-  }
+func (srv *Server) Serve() error {
+  mux := http.NewServeMux()
+  mux.HandleFunc("/api/v1/sign_up/", srv.SignUpHandler())
 
-  s.SignedUp[userEmail] = User {
-    Name: userName,
-    Email: userEmail,
+  s := http.Server {
+    Addr:           ":8080",
+    Handler:        mux,
+    ReadTimeout:    10 * time.Second,
+    WriteTimeout:   10 * time.Second,
+    MaxHeaderBytes: 1 << 20,
   }
-
-  return nil
+  fmt.Println("Listening on", s.Addr, "...")
+  return s.ListenAndServe()
 }
 
-func (s *Server) LogIn(userEmail string) (LogInToken, error) {
-  if _, exists := s.SignedUp[userEmail]; !exists {
-    return fmt.Errorf("User does not exist")
-  }
+func (s *Server) SignUp(userEmail Email, userName string) error {
+	if _, exists := s.SignedUp[userEmail]; exists {
+		return fmt.Errorf("User already exists")
+	}
 
-  uuidBytes := [4]byte{}
-  if _, err := rand.Read(uuidBytes[:]); err != nil {
-    return err
-  }
+	s.SignedUp[userEmail] = User{
+		Name:  userName,
+		Email: userEmail,
+	}
 
-  uuid := binary.BigEndian.Uint64(uuidBytes[:])
-  // TODO check collisions of the uuid and retry or crash
-
-  loginToken := LogInToken {
-    ValidUntil: time.Now().Add(72 * time.Hour),
-    Uuid: uuid,
-  }
-  return loginToken, nil
+	return nil
 }
 
-func (s *Server) SignInHandler() {
-  return func(w http.ResponseWriter, r *http.Request) {
-    if r.Method != http.MethodPut {
-      w.WriteHeader(400)
-      return
-    }
-    if err := r.ParseForm(); err != nil {
-      w.WriteHeader(500)
-      return
-    }
-    email := r.Form.Get("email")
-    if email == "" {
-      w.WriteHeader(401)
-      return
-    } else if !strings.EndsWith(email, "princeton.edu") {
-      // temporary only allow princeton email addresses
-      w.WriteHeader(401)
-      return
-    }
-    // TODO validate name
-    name := r.Form.Get("name")
-    user, err := s.SignUp(email, name)
-    if err != nil {
-      panic("TODO")
-    }
-    logInToken, err := s.LogIn(email, user)
-    if err != nil {
-      panic("TODO")
-    }
-    // TODO I forgot how to make this JSON encoded
-    if err = w.Write(logInToken); err != nil {
-      panic("TODO")
-    }
-    return
+type Uuid uint64
+
+func generateUuid() (Uuid, error) {
+	uuidBytes := [4]byte{}
+	if _, err := rand.Read(uuidBytes[:]); err != nil {
+		return Uuid(0), err
+	}
+
+	uuid := binary.BigEndian.Uint64(uuidBytes[:])
+  return Uuid(uuid), nil
+}
+
+func (s *Server) LogIn(userEmail Email) (LogInToken, error) {
+	if _, exists := s.SignedUp[userEmail]; !exists {
+		return LogInToken{}, fmt.Errorf("User does not exist")
+	}
+
+	// TODO check collisions of the uuid and retry or crash
+  uuid, err := generateUuid()
+  if err != nil {
+		return LogInToken{}, err
   }
+
+	loginToken := LogInToken{
+		ValidUntil: time.Now().Add(72 * time.Hour),
+		Uuid:       uuid,
+	}
+	return loginToken, nil
+}
+
+func (s *Server) SignUpHandler() func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodPut {
+			w.WriteHeader(400)
+			return
+		}
+		if err := r.ParseForm(); err != nil {
+			w.WriteHeader(500)
+			return
+		}
+		email, err := NewEmail(r.Form.Get("email"))
+		if err != nil {
+			w.WriteHeader(401)
+			return
+		}
+		// TODO validate name
+		name := r.Form.Get("name")
+		if err = s.SignUp(email, name); err != nil {
+			panic("TODO")
+		}
+		logInToken, err := s.LogIn(email)
+		if err != nil {
+			panic("TODO")
+		}
+		// TODO I forgot how to make this JSON encoded
+		enc := json.NewEncoder(w)
+		if err = enc.Encode(logInToken); err != nil {
+			panic("TODO")
+		}
+		return
+	}
 }
