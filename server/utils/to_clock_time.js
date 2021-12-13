f072e2cacfe5af8615c9cb27ae96f907f4d39600
julianknodt <julianknodt@gmail.com>
2021-12-12 23:43:10 -0500

    Add parsing clock time from out json

---
+++ server/utils/to_clock_time.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+
+const responses = JSON.parse(fs.readFileSync("out.json"));
+const times = responses.times;
+
+for (let k in times) {
+  const at = times[k];
+  console.log(k, at);
+}
+
